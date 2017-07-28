3f203754f31104d906cf4831429075df196aabde
DeliaCodes <deliacodes@gmail.com>
2017-07-27 21:23:05 -0700

    still subtly broken

--- app.js
+++ app.js
@@ -3,19 +3,18 @@ var data = {
 	items : []
 };
 
-var addItem = function(state, item) {
-    state.items.push(item);
+var addItem = function(data, item) {
+    data.items.push(item);
 };
 
-var renderList = function(state, element) {
-    var itemsHTML = state.items.map(function(item) {
-        return '<li>' + item + '</li>';
-    });
+var renderList = function(data, element) {
+    var itemsHTML = data.items.map(function(item) {
+        return '<li>' + '<span class="shopping-item">' + item + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>';
     element.html(itemsHTML);
 };
 
-$('.shopping-list-add').submit(function(event) {
+$('#shopping-list-add-input').submit(function(event) {
     event.preventDefault();
-    addItem(state, $('.shopping-list-add-input').val());
-    renderList(state, $('.shopping-list'));
+    addItem(data, $('.shopping-list').val());
+    renderList(data, $('.shopping-list'));
 });
