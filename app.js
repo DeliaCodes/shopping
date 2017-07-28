479f130175547cf0b5c4e15991b431efc7a097b7
DeliaCodes <deliacodes@gmail.com>
2017-07-28 06:43:09 -0700

    roughed out the delete and check portions

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
+'use strict';
 
+//first the variable to store the list
 var data = {
 	items : []
 };
 
+//Add function
 var addItem = function(data, item) {
     data.items.push(item);
 };
 
+//display function - sounds like the return could use the .closest method
 var renderList = function(data, element) {
     var itemsHTML = data.items.map(function(item) {
         return '<li>' + '<span class="shopping-item">' + item + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>';
     element.html(itemsHTML);
 };
 
+//captures submit data, adds it, renders it, maybe
 $('#shopping-list-add-input').submit(function(event) {
     event.preventDefault();
     addItem(data, $('.shopping-list').val());
     renderList(data, $('.shopping-list'));
 });
+
+//delete data function - do I need to call this?
+$('.shopping-item-delete').submit(function () {
+	event.preventDefault();
+	this.closest('.shopping-item').hide();
+
+});
+
+//check data function
+$('.shopping-item-toggle').submit(function(){
+	event.preventDefault();
+});
+
+//calls the delete data function and hopefully the check data function
+
